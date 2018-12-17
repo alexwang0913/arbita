9a9846d0ca7ba8e0e3c73e49aa558f201339e8f1
xu19951221 <45426866+xu19951221@users.noreply.github.com>
2018-12-18 06:22:22 +0800

    commit sth

--- server/app.js
+++ server/app.js
@@ -8,6 +8,33 @@ const trunks = require('trunks-log')
 const app = express();
 const http = require('http');
 
+
+// app.use(function(req, res, next) {
+//   var oneof = false;
+//   if(req.headers.origin) {
+//       res.header('Access-Control-Allow-Origin', req.headers.origin);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-method']) {
+//       res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+//       oneof = true;
+//   }
+//   if(req.headers['access-control-request-headers']) {
+//       res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+//       oneof = true;
+//   }
+//   if(oneof) {
+//       res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+//   }
+
+//   // intercept OPTIONS method
+//   if (oneof && req.method == 'OPTIONS') {
+//       res.send(200);
+//   }
+//   else {
+//       next();
+//   }
+// });
 app.use(cors())
 const logs = new trunks('', 'yellow', '')
 
@@ -18,7 +45,7 @@ const { apiRoutes } = require('./src/routes/index')
 mongoose.Promise = global.Promise
 
 // Connect to the database
-mongoose.connect('mongodb://localhost:27017/arbitrage')
+mongoose.connect('mongodb://localhost:27017/arbitrage', { useNewUrlParser: true })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
@@ -52,8 +79,8 @@ app.use(function(err, req, res, next) {
 
 const port = process.env.PORT || 5000;
 // app.listen(port);
-// const serverUrl = '127.0.0.1';
-const serverUrl = '138.68.107.2';
+const serverUrl = '127.0.0.1';
+// const serverUrl = '138.68.107.2';
 http.createServer(app).listen(port, serverUrl);
 
 logs.success('App running on http://localhost:{}', port)
