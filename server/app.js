f63d628132e23376cd8a2034309a88e67705703e
xu19951221 <45426866+xu19951221@users.noreply.github.com>
2018-12-19 01:06:04 +0800

    update something

--- server/app.js
+++ server/app.js
@@ -9,33 +9,33 @@ const app = express();
 const http = require('http');
 
 
-// app.use(function(req, res, next) {
-//   var oneof = false;
-//   if(req.headers.origin) {
-//       res.header('Access-Control-Allow-Origin', req.headers.origin);
-//       oneof = true;
-//   }
-//   if(req.headers['access-control-request-method']) {
-//       res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-//       oneof = true;
-//   }
-//   if(req.headers['access-control-request-headers']) {
-//       res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-//       oneof = true;
-//   }
-//   if(oneof) {
-//       res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-//   }
+app.use(function(req, res, next) {
+  var oneof = false;
+  if(req.headers.origin) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      oneof = true;
+  }
+  if(req.headers['access-control-request-method']) {
+      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+      oneof = true;
+  }
+  if(req.headers['access-control-request-headers']) {
+      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+      oneof = true;
+  }
+  if(oneof) {
+      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
 
-//   // intercept OPTIONS method
-//   if (oneof && req.method == 'OPTIONS') {
-//       res.send(200);
-//   }
-//   else {
-//       next();
-//   }
-// });
-app.use(cors())
+  // intercept OPTIONS method
+  if (oneof && req.method == 'OPTIONS') {
+      res.send(200);
+  }
+  else {
+      next();
+  }
+});
+// app.use(cors())
 const logs = new trunks('', 'yellow', '')
 
 // const index = require('./src/routes/index');
